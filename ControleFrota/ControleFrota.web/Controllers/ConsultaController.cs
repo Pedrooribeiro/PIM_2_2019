d35fd3735e30cafbec48c885e76ea0ac28fa3689
Pedro Ribeiro <pedro.henrique-ribeiro@hotmail.com>
2019-10-29 22:56:54 -0300

    adicionado todos menus de consulta da página principal

---
+++ ControleFrota/ControleFrota.web/Controllers/ConsultaController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleFrota.web.Controllers
+{
+    public class ConsultaController : Controller
+    {
+
+        [Authorize]
+        public ActionResult ConsultaMotorista()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult ConsultaMulta()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult ConsultaPeca()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult ConsultaVeiculo()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult ConsultaPneu()
+        {
+            return View();
+        }
+    }
+}
